2a32e7422e359660b97af453b5ae59fb81cfd09f
ST10255814 <ST10255814@vcconnect.edu.za>
2025-10-14 16:39:34 +0200

    refactor: Remove trailing slash from production API endpoint in CORS configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,7 @@ app.use(
           "http://127.0.0.1:5000",
           "http://localhost:5000",
           "http://localhost:3000",
-          "https://rentwiseproperty.onrender.com/",
+          "https://rentwiseproperty.onrender.com",
         ],
       },
     },
