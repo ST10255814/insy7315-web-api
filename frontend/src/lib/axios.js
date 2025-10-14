5783ad0a7aaa85a2bada03e00b8dd39d52595549
ST10255814 <ST10255814@vcconnect.edu.za>
2025-10-14 03:38:57 +0200

    feat: Implement registration API call and enhance form validation in Register component

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'https://127.0.0.1:5000',
+    baseURL: 'http://127.0.0.1:5000',
     headers: {'Content-Type': 'application/json'},
     withCredentials: true
 })
