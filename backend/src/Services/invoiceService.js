34855b6f24092985239b0d8b1ed2a480c0171f91
Gerhard Lemmer <ST10253994@vcconnect.edu.za>
2025-10-20 13:29:03 +0200

    fix: Correct tenant name property from 'lastName' to 'surname' in createInvoice function

--- backend/src/Services/invoiceService.js
+++ backend/src/Services/invoiceService.js
@@ -39,7 +39,7 @@ async function createInvoice(adminId, data){
 
         //create lease object
         const leaseObject = {
-            tenant: lease.tenant.firstName + " " + lease.tenant.lastName,
+            tenant: lease.tenant.firstName + " " + lease.tenant.surname,
             email: lease.tenant.email,
             propertyAddress: lease.listing.address,
         }
